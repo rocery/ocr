294384f913dc9612ea4f68dc2df7168eceffddf7
rocery <sastranuralamsyah@gmail.com>
2024-08-12 13:36:40 +0700

    test_

--- aeasyocr.py
+++ aeasyocr.py
@@ -1,28 +1,30 @@
 import easyocr
 import numpy as np
 import cv2
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
+import re
 
 reader = easyocr.Reader(['en'])
 plat = []
 
 def predict(frame):
     result = reader.readtext(frame)
-    global plat
-    reject = [" ", ".", ",", ":", "-", "/"]
-    
-    # Extract bounding boxes, texts, and confidence scores
     boxes = [line[0] for line in result]
     txts = [line[1] for line in result]
     scores = [line[2] for line in result]
     
+    reject = [".", ","]
+    global plat
     plat = []
     for data in txts:
         if any(char in data for char in reject):
-            continue  # Skip this data if it contains any of the reject characters
+            continue
         else:
-            plat.append(data)
-    
+            cleaned_string = re.sub(r'[^\w\s\n]', '', data)
+            cleaned_string = cleaned_string.upper()
+            plat.append(cleaned_string)
+        print("Data: {}, cleaned string: {}".format(data, cleaned_string))
+        
     boxes = [np.array(box, dtype=np.int32).reshape((-1, 1, 2)) for box in boxes]
     
     return [(box, txt, score) for box, txt, score in zip(boxes, txts, scores)]
@@ -46,7 +48,18 @@ def show_labels(frame, predictions):
     opencvimage = np.array(frame)
     return opencvimage
 
-if __name__ == '__main__':
+def for_image(img):
+    image = cv2.imread(img)
+    
+    predictions = []
+    predictions = predict(image)
+    frame = show_labels(image, predictions)
+    
+    cv2.imshow('OCR', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+def for_video():
     process_this_frame = 29
     cap = cv2.VideoCapture(0)
     predictions = []
@@ -58,10 +71,13 @@ if __name__ == '__main__':
             if process_this_frame % 30 == 0:
                 predictions = predict(frame)
             frame = show_labels(frame, predictions)
-            print(plat)
-            cv2.imshow('Face Recognition', frame)
-            print(plat)
+            # print(plat)
+            cv2.imshow('OCR', frame)
             if ord('q') == cv2.waitKey(10):
                 cap.release()
                 cv2.destroyAllWindows()
                 exit(0)
+
+if __name__ == '__main__':
+    img_dir = 'pic/contoh1.png'
+    for_image(img_dir)
